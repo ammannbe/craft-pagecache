2d4686a3fc6add7222159645c131186c49663bb6
Benjamin Ammann <26964239+ammannbe@users.noreply.github.com>
2023-05-13 14:08:18 +0200

    prevent loading jquery when using PageCacheVariable

--- src/variables/PageCacheVariable.php
+++ src/variables/PageCacheVariable.php
@@ -11,6 +11,7 @@
 namespace suhype\pagecache\variables;
 
 use Craft;
+use craft\web\View;
 use craft\helpers\Html;
 
 class PageCacheVariable
@@ -19,70 +20,23 @@ class PageCacheVariable
     {
         $rand = rand();
 
-        Craft::$app->getView()->registerJsWithVars(fn ($type) => <<<JS
-(() => {
-    fetch('/actions/users/session-info', { headers: { 'Accept': 'application/json' } })
-        .then((resp) => resp.json())
-        .then(function(data) {
-            const input = document.querySelector('[data-pagecache="{$rand}"]');
+        Craft::$app->getView()->registerJs(<<<JS
+    (() => {
+        fetch('/actions/users/session-info', { headers: { 'Accept': 'application/json' } })
+            .then((resp) => resp.json())
+            .then(function(data) {
+                const input = document.querySelector('[data-pagecache="{$rand}"]');
 
-            if (input) {
-                input.value = data.csrfTokenValue;
-            }
-        });
-})();
-JS, [static::class]);
+                if (input) {
+                    input.value = data.csrfTokenValue;
+                }
+            });
+    })();
+JS, View::POS_END);
 
         $tokenName = Craft::$app->getConfig()->getGeneral()->csrfTokenName;
         return Html::hiddenInput($tokenName, null, [
             'data-pagecache' => $rand,
         ]);
-    }
-
-    /**
-     * Get the resulting posts.
-     *
-     * This method is deprecated and will be removed in the next release.
-     * Use [[SocialFeedVariable::posts()]] instead.
-     *
-     * @param array $conditions Some additional query conditions.
-     * @param ?int $limit If not provided, use the limit from the settings.
-     * @param ?array $orderBy If not provided, order by the feed date created.
-     * @return array<ElementInterface>
-     */
-    public function all($conditions = [], $limit = null, $orderBy = ['homm_socialfeeds.feedDateCreated' => SORT_DESC]): array
-    {
-        return $this->posts($conditions, $limit, $orderBy)->all();
-    }
-
-    /**
-     * Get the social feeds query
-     *
-     * @param array $conditions Some additional query conditions.
-     * @param ?int $limit If not provided, use the limit from the settings.
-     * @param ?array $orderBy If not provided, order by the feed date created.
-     * @return ElementQueryInterface
-     */
-    public function posts($conditions = [], $limit = null, $orderBy = ['homm_socialfeeds.feedDateCreated' => SORT_DESC]): ElementQueryInterface
-    {
-        $firstKey = array_key_first($conditions);
-        $query = SocialFeed::find();
-        $limit = $limit ?? HOMMSocialFeed::$plugin->getSettings()->numberOfFeeds;
-
-        foreach ($conditions as $key => $condition) {
-            if ($key == $firstKey) {
-                $query = $query->where($condition);
-                continue;
-            }
-            $query = $query->andWhere($condition);
-        }
-
-        $query = $query->limit($limit);
-
-        if ($orderBy !== null) {
-            $query = $query->orderBy($orderBy);
-        }
-
-        return $query;
     }
 }
